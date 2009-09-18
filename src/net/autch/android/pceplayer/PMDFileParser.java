d29043e25abbdf657bee7e309c8932bf24a638e8
autch <autch@96b12572-75c9-4af0-9ecf-4ada29f540e6>
2009-09-18 06:42:40 +0000

    * スレッドは使ってないので削除 * 曲を調べてるときはプログレス表示 * PMD ファイル解析を軽量化。メモリ使用量は減ってないけど。
    git-svn-id: file:///home/autch/var/lib/svn/pceplayer/trunk@5 96b12572-75c9-4af0-9ecf-4ada29f540e6

--- src/net/autch/android/pceplayer/PMDFileParser.java
+++ src/net/autch/android/pceplayer/PMDFileParser.java
@@ -1,69 +1,51 @@
 package net.autch.android.pceplayer;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
 
 public class PMDFileParser {
 
+	private static int getShort(byte[] b, int offset) {
+		return b[offset] | (b[offset + 1] << 8);
+	}
+
 	public static String[] getTitleInfo(File f) throws IOException {
 		String[] titles = new String[] { "", "" };
-		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		byte[] raw = new byte[8192];
+		int offset = 0;
+
 		FileInputStream is = new FileInputStream(f);
-		FileChannel ch = is.getChannel();
-	
-		buffer.order(ByteOrder.LITTLE_ENDIAN);
-		
-		ch.position(0);
-		ch.read(buffer);
-		buffer.rewind();
-		
-		int parts = buffer.get();	// 0, or # of parts
+		try {
+			is.read(raw);
+		} finally {
+			is.close();
+		}
+
+		int parts = raw[0];	// 0, or # of parts
+		offset++;
 		if(parts == 0) {
-			parts = buffer.get();
+			parts = raw[1];
+			offset++;
 		}
-		for(; parts > 0; parts--) {
-			buffer.getShort();	// addr to part data
-		}
-		buffer.getShort();	// addr to drum definition
-		
+		offset += 2 * parts + 2;
+
 		int adrTitle, adrTitle2;
 		int lenTitle, lenTitle2;
-		adrTitle = buffer.getShort();
-		adrTitle2 = buffer.getShort();
-		
+		adrTitle = getShort(raw, offset);
+		adrTitle2 = getShort(raw, offset + 2);
+
 		if(adrTitle > 0) {
-			buffer.position(adrTitle);
-			for(lenTitle = 0; buffer.get() != 0x00; lenTitle++)
+			for(lenTitle = 0; raw[adrTitle + lenTitle] != 0x00; lenTitle++)
 				;
-			buffer.position(adrTitle);
-			byte[] byTitle = new byte[lenTitle];
-			buffer.get(byTitle);
-			titles[0] = bytesToString(byTitle);
+			titles[0] = new String(raw, adrTitle, lenTitle, "Shift_JIS");
 		}
 		if(adrTitle2 > 0) {
-			buffer.position(adrTitle2);
-			for(lenTitle2 = 0; buffer.get() != 0x00; lenTitle2++)
+			for(lenTitle2 = 0; raw[adrTitle2 + lenTitle2] != 0x00; lenTitle2++)
 				;
-			buffer.position(adrTitle2);
-			byte[] byTitle2 = new byte[lenTitle2];
-			buffer.get(byTitle2);
-			titles[1] = bytesToString(byTitle2);
+			titles[1] = new String(raw, adrTitle2, lenTitle2, "Shift_JIS");
 		}
-		
+
 		return titles;
-	}
-	
-	private static String bytesToString(byte[] b) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(b), Charset.forName("Shift_JIS")), 1024);
-		return br.readLine();
 	}
 }
--- src/net/autch/android/pceplayer/SelectSong.java
+++ src/net/autch/android/pceplayer/SelectSong.java
@@ -6,47 +6,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 public class SelectSong extends ListActivity {
-	ArrayList<HashMap<String, String>> files;
+	private final ArrayList<HashMap<String, String>> files = new ArrayList<HashMap<String, String>>();
+	private final Handler handler = new Handler();
+	private ProgressDialog dialog; 
+
+	private final DirDiver.Callback enumFiles = new DirDiver.Callback() {
+		public boolean process(File f) {
+			try {
+				String[] titles = PMDFileParser.getTitleInfo(f);
+				String realTitle = titles[0];
+				if(realTitle == null || realTitle.length() == 0) {
+					realTitle = f.getName();
+				}
+
+				HashMap<String, String> map = new HashMap<String, String>();
+				map.put("filename", f.toString());
+				map.put("title", realTitle);
+				map.put("title2", titles[1]);
+				files.add(map);
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+	}; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		files = new ArrayList<HashMap<String, String>>();
 
-		DirDiver diver = new DirDiver("/sdcard", ".pmd");
-		diver.setCallback(new DirDiver.Callback() {
-			public boolean process(File f) {
-				try {
-					String[] titles = PMDFileParser.getTitleInfo(f);
-					String realTitle = titles[0];
-					if(realTitle == null || realTitle.length() == 0) {
-						realTitle = f.getName();
+		files.clear();
+
+		dialog = ProgressDialog.show(this, null, "曲を探しています...", true, false);
+
+		new Thread(new Runnable() {
+			public void run() {
+				DirDiver diver = new DirDiver("/sdcard", ".pmd");
+				diver.setCallback(enumFiles);
+				diver.start();
+
+				handler.post(new Runnable() {
+					public void run() {
+						SimpleAdapter adapter = new SimpleAdapter(SelectSong.this, files, android.R.layout.simple_list_item_2,
+								new String[] { "title", "title2" }, new int[] { android.R.id.text1, android.R.id.text2 });
+						setListAdapter(adapter);
+						dialog.dismiss();
 					}
-
-					HashMap<String, String> map = new HashMap<String, String>();
-					map.put("filename", f.toString());
-					map.put("title", realTitle);
-					map.put("title2", titles[1]);
-					files.add(map);
-				} catch (IOException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-				}
-				return true;
+				});
 			}
-		});
-		diver.start();
-
-		SimpleAdapter adapter = new SimpleAdapter(this, files, android.R.layout.simple_list_item_2,
-				new String[] { "title", "title2" }, new int[] { android.R.id.text1, android.R.id.text2 });
-		setListAdapter(adapter);
+		}).start();
 	}
 
 	@Override
